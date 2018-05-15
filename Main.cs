addd935a06a4c8601cba18845fa888d3fcba19a2
likeaguest <jan.ly@outlook.at>
2018-05-15 16:57:48 +0200

    Spawning near gate doesn't prevent you from going through anymore.

--- Main.cs
+++ Main.cs
@@ -11,7 +11,6 @@ namespace DoorFix {
         DoorCreator door;
         DoorCreator gate;
 
-
         public DoorFix() {
             API.onResourceStart += FixDoors;
             API.onResourceStop += Reset;
@@ -21,12 +20,17 @@ namespace DoorFix {
             remover = new ObjectRemover(-844827165, new Vector3(-875.4845f, 18.12612f, 44.4434), 1f);
             door = new DoorCreator(724862427, new Vector3(-883.03f, 21.98f, 45.50705f), new Vector3(1.556294E-05, -3.07506E-05, -29.79311), 1f);
             gate = new DoorCreator(-2125423493, new Vector3(-875.4, 18.07, 44.45), new Vector3(0, 0, 149.9994), 1f);
+
+            API.shared.createObject(door.model, door.location, door.rotation);
+            API.shared.createObject(gate.model, gate.location, gate.rotation);
+
             remover.CreateColShape();
             door.CreateColShape();
             gate.CreateColShape();
 
             foreach (Client player in API.getAllPlayers()) {
                 if (player.position.DistanceTo2D(remover.location) <= 25f) {
+					API.shared.delay(200, true, () => API.shared.sendNativeToPlayer(player, 0xF82D8F1926A02C3D, model, location.X, location.Y, location.Z, false, 0f, false));
                     API.shared.deleteObject(player, remover.location, remover.model, remover.radius);
                 }
             }
